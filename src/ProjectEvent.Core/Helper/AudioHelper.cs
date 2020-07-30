c64721f5b41d7d309f378c22533c7ed71dca144c
thelittlepandaisbehind <root@thelittlepandaisbehind.com>
2020-07-30 22:14:25 +0800

    添加音频管理

---
+++ src/ProjectEvent.Core/Helper/AudioHelper.cs
@@ -0,0 +1,55 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEvent.Core.Helper
+{
+    public static class AudioHelper
+    {
+        static CoreAudioController controller;
+        private static void Init()
+        {
+            if (controller == null)
+            {
+                controller = new CoreAudioController();
+            }
+        }
+        /// <summary>
+        /// 获取当前声音播放设备的音量
+        /// </summary>
+        /// <returns></returns>
+        public static double GetMasterVolume()
+        {
+            try
+            {
+                Init();
+                var device = controller.DefaultPlaybackDevice;
+                return device.Volume;
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error(e.ToString());
+            }
+            return -1;
+        }
+        /// <summary>
+        /// 设置当前声音播放设备的音量
+        /// </summary>
+        /// <param name="value"></param>
+        public static void SetMasterVolume(double value)
+        {
+            try
+            {
+                Init();
+                var device = controller.DefaultPlaybackDevice;
+                device.Volume = value;
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error(e.ToString());
+            }
+
+        }
+    }
+}
