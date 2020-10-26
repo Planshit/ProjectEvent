9e42f9253a7eeb0cb9262c842dd7d9f80e03be86
abadgirlnoa <abadgirlnoa@null.com>
2020-10-26 22:08:36 +0800

    修复：周期事件偶尔不触发的bug

--- src/ProjectEvent.Core/Services/TimerService.cs
+++ src/ProjectEvent.Core/Services/TimerService.cs
@@ -7,7 +7,7 @@ using System.Timers;
 
 namespace ProjectEvent.Core.Services
 {
-    public class TimerService : ITimerService, IDisposable
+    public class TimerService : ITimerService
     {
         private Dictionary<int, Timer> _timers;
         private Dictionary<int, int> _timerRunCount;
@@ -86,15 +86,16 @@ namespace ProjectEvent.Core.Services
             Debug.WriteLine("任务在：" + seconds + "秒后执行");
             timer.Elapsed += (e, c) =>
             {
-                action?.Invoke();
-                timerClosedAction?.Invoke();
+                int nextCount = GetRunCount(id) + 1;
                 SetRunCount(id);
 
-                int nextCount = GetRunCount(id) + 1;
                 if (num > 0 && nextCount > num)
                 {
                     Close(id);
                 }
+
+                action?.Invoke();
+                timerClosedAction?.Invoke();
             };
 
             _timerRunCount.Add(id, 0);
@@ -151,47 +152,7 @@ namespace ProjectEvent.Core.Services
             }
         }
 
-        #region IDisposable Support
-        private bool disposedValue = false; // 要检测冗余调用
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: 释放托管状态(托管对象)。
-                    foreach (var timer in _timers.Values)
-                    {
-                        timer.Stop();
-                        timer.Close();
-                    }
-                }
-
-                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
-                // TODO: 将大型字段设置为 null。
-                _timers = null;
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
-        // ~TimerService()
-        // {
-        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-        //   Dispose(false);
-        // }
-
-        // 添加此代码以正确实现可处置模式。
-        public void Dispose()
-        {
-            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-            Dispose(true);
-            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
 
 
     }
